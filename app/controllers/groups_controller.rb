3b5718b3308a2896a2fc588906a8ce59c0d89eb1
周泽宇 <zhouzeyuinchina@gmail.com>
2018-03-20 09:19:36 +0800

    implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,13 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+  
 end
