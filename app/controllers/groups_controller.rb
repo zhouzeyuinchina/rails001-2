e1b3d2d0dad4b89f9bc4b16992722d90d9c1ef47
周泽宇 <zhouzeyuinchina@gmail.com>
2018-03-20 09:31:14 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,6 +24,15 @@ class GroupsController < ApplicationController
     end
   end
 
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end 
+
   private
 
   def group_params
