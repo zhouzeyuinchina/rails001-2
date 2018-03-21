b811a731bbd3278950711542a3493e8a558d29e5
周泽宇 <zhouzeyuinchina@gmail.com>
2018-03-21 15:04:09 +0800

    complete edit & delete of account post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create,:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -20,9 +20,36 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.destroy
+    redirect_to account_posts_path , alert: "Post deleted"
+  end 
+
   private
 
   def post_params
     params.require(:post).permit(:content)
-  end 
+  end
 end
